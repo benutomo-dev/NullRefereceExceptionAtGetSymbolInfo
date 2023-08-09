bf27eb5c061c14e98d4c97d79987d815b1ede6d1
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2023-08-09 14:35:54 +0900

    プロジェクト ファイルを追加します。

---
+++ NullRefereceExceptionAtGetSymbolInfo/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace NullRefereceExceptionAtGetSymbolInfo;
+
+public class Class1
+{
+    void Method()
+    {
+        var x = Enumerable.Empty<int>().MySelect((v, i) => i);
+    }
+}
+
+file static class ImmutableArrayExtensionsClass1
+{
+    internal static IEnumerable<U> MySelect<T, U>(this IEnumerable<T> @this, Func<T, int, U> selector)
+    {
+        return Enumerable.Empty<U>();
+    }
+}
---
+++ NullRefereceExceptionAtGetSymbolInfoAnalyzer/Analyzer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace NullRefereceExceptionAtGetSymbolInfoAnalyzer;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class Analyzer : DiagnosticAnalyzer
+{
+    internal static DiagnosticDescriptor s_diagnosticDescriptor_XX0001 = new DiagnosticDescriptor(
+        "XX0001",
+        "Xx",
+        "Xx。",
+        "Usage",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+        s_diagnosticDescriptor_XX0001
+        );
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(DetectDiscardOfCollectionOperationResult, SyntaxKind.SimpleMemberAccessExpression);
+    }
+
+    private static void DetectDiscardOfCollectionOperationResult(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is not MemberAccessExpressionSyntax memberAccessExpressionSyntax)
+        {
+            return;
+        }
+
+        // NullReferenceException!!
+        context.SemanticModel.GetSymbolInfo(memberAccessExpressionSyntax, context.CancellationToken);
+    }
+}
