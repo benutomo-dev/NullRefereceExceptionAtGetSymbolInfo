847de02bf501a341b537b03c168951a4a8bf01fb
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2023-08-09 14:35:54 +0900

    プロジェクト ファイルを追加します。

---
+++ NullRefereceExceptionAtGetSymbolInfo/Class2.cs
@@ -0,0 +1,17 @@
+﻿namespace NullRefereceExceptionAtGetSymbolInfo;
+
+public class Class2
+{
+    void Method()
+    {
+        var x = Enumerable.Empty<int>().MySelect(v => v);
+    }
+}
+
+file static class ImmutableArrayExtensionsClass2
+{
+    internal static IEnumerable<U> MySelect<T, U>(this IEnumerable<T> @this, Func<T, U> selector)
+    {
+        return Enumerable.Empty<U>();
+    }
+}
